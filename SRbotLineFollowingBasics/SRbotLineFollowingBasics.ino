140fc698c11749a5ac8542685efcbd4fefc4debb
willjox <william.jox@gmail.com>
2021-11-15 12:05:39 +0100

    Skeleton for linefollowing functionality

---
+++ SRbotLineFollowingBasics/SRbotLineFollowingBasics.ino
@@ -0,0 +1,31 @@
+
+#define leftLineSensor  1
+#define rightLineSensor 2
+#define middleLineSensor 3
+#define leftTurnSensor 4
+#define rightTurnSensor 5
+
+int linePins[] = {leftLineSensor, rightLineSensor , middleLineSensor};
+int turnPins[] = {leftTurnSensor, rightTurnSensor};
+int lineStates[];
+int turnStates[];
+
+void getState(int sensors[]) {
+
+}
+
+void controlAlgorithm () {
+
+}
+
+void followLine() {
+
+}
+
+void detectTurns() {
+
+}
+
+void setup {}
+
+void loop {}
