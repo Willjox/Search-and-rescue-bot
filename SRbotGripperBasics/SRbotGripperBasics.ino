45ce34876d7a7ea760d7f78893a8f9c4b71f93cc
willjox <william.jox@gmail.com>
2021-11-22 17:24:09 +0100

    Gripper proof of concept

--- SRbotGripperBasics/SRbotGripperBasics.ino
+++ SRbotGripperBasics/SRbotGripperBasics.ino
@@ -1,23 +1,64 @@
 #include <Servo.h>
 Servo gripperServo;
 Servo rotateServo;
+const int startButton = 13;
+const int gripServoIO = 8;
+const int rotateServoIO = 9;
 void setup() {
  Serial.begin(9600);
  Serial.println("starting setup");
- rotateServo.attach(12);
- gripperServo.attach(5);
+ rotateServo.attach(rotateServoIO);
+ gripperServo.attach(gripServoIO);
+ pinMode(startButton,INPUT);
+ gripperServo.write(90);
+ rotateServo.write(180);
+
+Serial.println("Starting");
  Serial.println("Finished setup");
 }
 
 void loop() {
   int pos;
-  for (pos = 0; pos <= 180; pos += 1) {
-    rotateServo.write(pos);
+  int rotatePos = 180;
+  Serial.println("waiting for start button, grip");
+  while (digitalRead(startButton)== LOW) {
+    delay(100);
+  }
+
+  for (pos = 90; pos >= 0; pos -= 1) {
+    gripperServo.write(pos);
+    Serial.println(pos);
     delay(15);
   }
-  for (pos = 180; pos >= 0; pos -= 1) { 
-    rotateServo.write(pos);
-    delay(15);
+  Serial.println("waiting for button, raise");
+  while (digitalRead(startButton)== LOW) {
+   delay(100);
+  }
+
+  for (rotatePos; rotatePos >= 60; rotatePos -= 1) {
+    rotateServo.write(rotatePos);
+    delay(30);
+    Serial.println(rotatePos);
+  }
+  Serial.println("waiting for button, release");
+  while (digitalRead(startButton)== LOW) {
+   delay(100);
+  }
+
+  for (pos; pos <= 90; pos += 1) {
+      gripperServo.write(pos);
+      Serial.println(pos);
+      delay(30);
+  }
+  Serial.println("waiting for button, lower");
+  while (digitalRead(startButton)== LOW) {
+   delay(100);
+  }
+
+    for (rotatePos; rotatePos <= 180; rotatePos += 1) {
+    rotateServo.write(rotatePos);
+    delay(30);
+    Serial.println(rotatePos);
   }
 
 }
