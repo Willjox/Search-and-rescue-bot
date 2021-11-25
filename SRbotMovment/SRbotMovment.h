5b20ca8cccffc1ed4cd8512343ce35769a5b0f59
willjox <william.jox@gmail.com>
2021-11-25 18:21:49 +0100

    unsaved stuff

--- SRbotMovment/SRbotMovment.h
+++ SRbotMovment/SRbotMovment.h
@@ -1,6 +1,37 @@
+#ifndef SRbotMovment_h
+#define SRbotMovment_h
 
-void setup() {
+#include "Arduino.h"
+#include "Servo.h"
+#include "QTRSensors.h"
+
+
+
+class SRbotMovment
+{
+public:
+  SRbotMovment();
+  boolean followLine();
+
+
+private:
+  QTRSensors turnDetectors;
+  QTRSensors lineFollower;
+  Servo leftServo;
+  Servo rightServo;
+  const float kp = 0.01;
+  const float kd = 0,02;
+  const uint8_t linePins[3] = {4 , 3 , 3};
+  int lastError;
+  int leftBaseSpeed;
+  int rightBaseSpeed;
+
+  void adjustSpeed(int left, int right);
+  void servoOutput(int left , int right);
+  void control();
+  void loop();
+  int detectTurn();
+
 }
 
-void loop() {
-}
+#endif
